645affb2904c8eec4322632f4b39136b85af8053
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-02 07:06:10 +0900

    feature/baseball (#3)

--- java-baseball-6/src/test/java/baseball/ApplicationTest.java
+++ java-baseball-6/src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,14 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -14,17 +16,56 @@ class ApplicationTest extends NsTest {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    assertThat(output()).contains("낫싱", "3스트라이크",
+                            "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", "1볼 1스트라이크", "3스트라이크",
+                            "게임 종료");
                 },
                 1, 3, 5, 5, 8, 9
         );
     }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
+    void 게임종료_후_재시작_시_유효하지_않은_입력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThrows(IllegalArgumentException.class, () -> run("246", "135", "3"));
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 예외_테스트_자리수_4이상() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1234"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_자리수_2이하() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_0이_포함된_자연수() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1204"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_수가_아닌_문자가_포함() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("124 "))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_중복된_숫자_포함() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("122"))
+                .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
