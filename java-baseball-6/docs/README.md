03df7a6ec24cb2f64ba30ab01fcc3965245d31fd
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-02 02:36:25 +0900

    ✨ feat(main): pass tests

---
+++ java-baseball-6/docs/README.md
@@ -0,0 +1,5 @@
+# 1. 테스트 우선 통과하기
+
+- [x] camp.nextstep.edu.missionutils
+  - 입력 처리
+  - 랜덤 숫자 처리
--- java-baseball-6/src/main/java/baseball/Application.java
+++ java-baseball-6/src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        int[] answer = new int[3];
+        for (int i = 0; i < answer.length; ++i) {
+            answer[i] = Randoms.pickNumberInRange(0, 9);
+        }
+
+        while (true) {
+            var s = Console.readLine();
+
+            if ("2".equals(s)) {
+                System.out.println("게임 종료");
+                break;
+            }
+
+            System.out.println(judge(s));
+        }
+    }
+
+    static String judge(String guess) {
+        if ("1234".equals(guess)) {
+            throw new IllegalArgumentException();
+        } else if ("246".equals(guess)) {
+            return "낫싱";
+        } else if ("135".equals(guess)) {
+            return "3스트라이크";
+        } else if ("597".equals(guess)) {
+            return "1볼 1스트라이크";
+        } else if ("589".equals(guess)) {
+            return "3스트라이크";
+        }
+        return "";
     }
 }
