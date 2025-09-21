da2e9555299aee7200d37f4c239490a2042d845e
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-22 05:15:11 +0900

    🔬 test(Lotto): 로또 번호 유효성 검사 추가

--- java-lotto-6/src/test/java/lotto/LottoTest.java
+++ java-lotto-6/src/test/java/lotto/LottoTest.java
@@ -22,5 +22,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("1부터 45 사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
