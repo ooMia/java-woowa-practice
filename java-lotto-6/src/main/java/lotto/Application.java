70dc4025981e5847a153c4744a366a5838f4574a
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-24 21:46:54 +0900

    ♻️ refactor(Application): Controller 인스턴스 생성 방식 변경 및 run 메서드 호출 제거

--- java-lotto-6/src/main/java/lotto/Application.java
+++ java-lotto-6/src/main/java/lotto/Application.java
@@ -2,9 +2,17 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
+        var view = new lotto.view.View();
+        var api = new Controller(view);
+
         try {
-            Controller controller = new Controller(new lotto.view.View());
-            controller.run();
+            var money = api.inputMoney();
+            var lottos = lotto.model.Lotto.purchase(money);
+            view.printPurchasedLottos(lottos);
+
+            var winningNumbers = api.inputWinningNumbers();
+            var bonusNumber = api.inputBonusNumber(winningNumbers);
+            view.printSummary(lottos, winningNumbers, bonusNumber);
         } catch (final Exception ignore) {
             // unhandled exceptions
         }
