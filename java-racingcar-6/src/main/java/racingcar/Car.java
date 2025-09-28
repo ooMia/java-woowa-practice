e86ad23f5329b7c1a082618bd346694619aa5de6
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-28 23:28:22 +0900

    ✨ feat(Car): Car 클래스에 단순 getter 메서드 추가

--- java-racingcar-6/src/main/java/racingcar/Car.java
+++ java-racingcar-6/src/main/java/racingcar/Car.java
@@ -15,4 +15,12 @@ public class Car {
             ++position;
         }
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
