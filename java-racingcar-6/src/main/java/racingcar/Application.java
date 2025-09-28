9b1d48f6c580d17fcfb237fa20b1fcf3567129d9
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-28 22:36:02 +0900

    ✨ feat(Application): 메인 함수 호출 흐름 구현

--- java-racingcar-6/src/main/java/racingcar/Application.java
+++ java-racingcar-6/src/main/java/racingcar/Application.java
@@ -2,6 +2,26 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        var bw = new java.io.BufferedWriter(new java.io.OutputStreamWriter(System.out));
+        var view = new racingcar.View(bw);
+        var api = new racingcar.Controller(view);
+
+        try {
+            var names = api.inputCarNames();
+            var nRounds = api.inputNumberOfTrials();
+
+            // var game = new racingcar.Game(names);
+            var game = api.createGame(names);
+            for (int i = 0; i < nRounds; ++i) {
+                // game.moveCars();
+                api.playRound(game);
+                view.printStatus(game);
+            }
+
+            view.printWinners(game);
+        } catch (final Exception ignore) {
+            // unhandled exceptions
+            // exception test will fail with exception without this block
+        }
     }
 }
