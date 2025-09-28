d6c73638b6adfccfdd5c97cd0acff5070c2f9776
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-28 23:28:49 +0900

    ✨ feat(Game): 게임 클래스에 자동차 목록 및 우승자 반환 메서드 추가

--- java-racingcar-6/src/main/java/racingcar/Game.java
+++ java-racingcar-6/src/main/java/racingcar/Game.java
@@ -16,4 +16,23 @@ public class Game {
             car.move(camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9));
         }
     }
+
+    public Car[] getCars() {
+        return cars;
+    }
+
+    public Car[] getWinners() {
+        var winners = new java.util.ArrayList<Car>();
+        winners.add(cars[0]);
+
+        for (var car : cars) {
+            int standard = winners.get(0).getPosition();
+            int challenger = car.getPosition();
+
+            if (challenger < standard) continue;
+            if (challenger > standard) winners.clear();
+            winners.add(car);
+        }
+        return winners.toArray(new Car[0]);
+    }
 }
