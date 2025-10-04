3c017e91bd84a3a1b619e043bf7679ffa08a0019
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-04 15:17:48 +0900

    ♻️ refactor(model.ErrorCode): 예외 처리 메서드 개선

--- java-christmas-6/src/main/java/christmas/model/ErrorCode.java
+++ java-christmas-6/src/main/java/christmas/model/ErrorCode.java
@@ -6,7 +6,7 @@ public enum ErrorCode {
     // 주문 메뉴
     MENU_NOT_FOUND("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     MENU_COUNT_NOT_NUMBER("유효하지 않은 주문입니다. 다시 입력해 주세요."),
-    MENU_COUNT_NOT_POSITIVE("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    MENU_COUNT_TOO_LOW("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     MENU_FORMAT_INVALID("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     // 전체 주문
     ORDER_MENU_DUPLICATE("유효하지 않은 주문입니다. 다시 입력해 주세요."),
@@ -19,7 +19,7 @@ public enum ErrorCode {
         this.message = message;
     }
 
-    public static IllegalArgumentException exception(ErrorCode code) {
-        return new IllegalArgumentException(code.message);
+    public IllegalArgumentException exception() {
+        return new IllegalArgumentException(message);
     }
 }
