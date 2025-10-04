a3ed43e4714e77ddf0561dc8da1f398362d159b1
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-04 17:39:54 +0900

    🔬 test(ApplicationTest): 새로운 테스트 케이스 추가 및 출력 검증

--- java-christmas-6/src/test/java/christmas/ApplicationTest.java
+++ java-christmas-6/src/test/java/christmas/ApplicationTest.java
@@ -3,6 +3,8 @@ package christmas;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -22,6 +24,43 @@ class ApplicationTest extends NsTest {
                     "<총혜택 금액>",
                     "<할인 후 예상 결제 금액>",
                     "<12월 이벤트 배지>"
+            );
+        });
+    }
+
+    @Test
+    void 예제_1() {
+        assertSimpleTest(() -> {
+            run("26", "타파스-1,제로콜라-1");
+            assertThat(output()).contains(
+                    String.join(LINE_SEPARATOR, List.of("<주문 메뉴>", "타파스 1개", "제로콜라 1개")),
+                    String.join(LINE_SEPARATOR, List.of("<할인 전 총주문 금액>", "8,500원")),
+                    String.join(LINE_SEPARATOR, List.of("<증정 메뉴>", "없음")),
+                    String.join(LINE_SEPARATOR, List.of("<혜택 내역>", "없음")),
+                    String.join(LINE_SEPARATOR, List.of("<총혜택 금액>", "0원")),
+                    String.join(LINE_SEPARATOR, List.of("<할인 후 예상 결제 금액>", "8,500원")),
+                    String.join(LINE_SEPARATOR, List.of("<12월 이벤트 배지>", "없음"))
+            );
+        });
+    }
+
+    @Test
+    void 예제_2() {
+        assertSimpleTest(() -> {
+            run("3", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
+            assertThat(output()).contains(
+                    String.join(LINE_SEPARATOR, List.of("<주문 메뉴>",
+                            "티본스테이크 1개", "바비큐립 1개", "초코케이크 2개", "제로콜라 1개")),
+                    String.join(LINE_SEPARATOR, List.of("<할인 전 총주문 금액>", "142,000원")),
+                    String.join(LINE_SEPARATOR, List.of("<증정 메뉴>", "샴페인 1개")),
+                    String.join(LINE_SEPARATOR, List.of("<혜택 내역>",
+                            "크리스마스 디데이 할인: -1,200원",
+                            "평일 할인: -4,046원",
+                            "특별 할인: -1,000원",
+                            "증정 이벤트: -25,000원")),
+                    String.join(LINE_SEPARATOR, List.of("<총혜택 금액>", "-31,246원")),
+                    String.join(LINE_SEPARATOR, List.of("<할인 후 예상 결제 금액>", "135,754원")),
+                    String.join(LINE_SEPARATOR, List.of("<12월 이벤트 배지>", "산타"))
             );
         });
     }
