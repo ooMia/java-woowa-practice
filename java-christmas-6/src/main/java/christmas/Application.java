66ad490d4acdb960a94977b968b5eab0b8d0a752
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-04 15:43:05 +0900

    ✨ feat(application): 애플리케이션의 주요 기능 구현 및 초기화 로직 추가

--- java-christmas-6/src/main/java/christmas/Application.java
+++ java-christmas-6/src/main/java/christmas/Application.java
@@ -2,6 +2,19 @@ package christmas;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        var bw = new java.io.BufferedWriter(new java.io.OutputStreamWriter(System.out));
+        var inputView = new christmas.view.InputView(bw);
+        var outputView = new christmas.view.OutputView(bw);
+        var api = new christmas.Controller(inputView, outputView);
+
+        try {
+            api.greet();
+            var visitDate = api.readVisitDate();
+            var order = api.readOrder();
+            api.printOrder(order);
+            api.printDiscountSummary(order, visitDate);
+        } catch (final Exception ignore) {
+            // fallback
+        }
     }
 }
