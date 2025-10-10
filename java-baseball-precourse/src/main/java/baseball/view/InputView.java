d1abcc33e974821709554529964d133a24114a6c
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-10 12:24:34 +0900

    🐛 fix(view): 모든 입력 정보를 누수 없이 저장

--- java-baseball-precourse/src/main/java/baseball/view/InputView.java
+++ java-baseball-precourse/src/main/java/baseball/view/InputView.java
@@ -1,6 +1,7 @@
 package baseball.view;
 
 import baseball.Constant;
+import baseball.ErrorCode;
 import baseball.model.in.ExitCode;
 import baseball.model.in.Guess;
 import baseball.model.in.SampleInput;
@@ -21,10 +22,7 @@ public class InputView {
     }
 
     public Guess inputGuess() {
-        var line = Console.readLine();
-        int[] ns = new int[Constant.자리_수_제한];
-        for (int i = 0; i < ns.length; ++i)
-            ns[i] = Integer.parseInt(String.valueOf(line.charAt(i)));
+        int[] ns = readInts(); // 기본 예외 테스트를 참조하면, 모든 입력이 명시적으로 변환되어야 한다
         return new Guess(ns);
     }
 
@@ -35,5 +33,17 @@ public class InputView {
     public ExitCode inputExitCode() {
         var number = Integer.parseInt(Console.readLine());
         return new ExitCode(number);
+    }
+
+    private int[] readInts() {
+        try {
+            var line = Console.readLine();
+            int[] res = new int[line.length()];
+            for (int i = 0; i < res.length; ++i)
+                res[i] = Integer.parseInt(String.valueOf(line.charAt(i)));
+            return res;
+        } catch (Exception e) {
+            throw ErrorCode.INPUT_INVALID.exception();
+        }
     }
 }
