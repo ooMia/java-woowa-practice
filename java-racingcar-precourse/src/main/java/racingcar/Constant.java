d1000b41cfdfc4a117ff5259a63b7f7fda63cfae
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-11 20:09:48 +0900

    🛠️ chore(Constant): 상수 정리

--- java-racingcar-precourse/src/main/java/racingcar/Constant.java
+++ java-racingcar-precourse/src/main/java/racingcar/Constant.java
@@ -3,6 +3,8 @@ package racingcar;
 import racingcar.util.ExceptionHandler;
 
 public final class Constant {
+    // 잘못된 입력 // #L47
+    public static final Class<IllegalArgumentException> 잘못된_입력_예외 = IllegalArgumentException.class;
     public static final String ERROR_PREFIX = "[ERROR]";
     static {
         ExceptionHandler.setErrorPrefix(ERROR_PREFIX);
@@ -10,4 +12,17 @@ public final class Constant {
 
     private Constant() {
     }
+
+    // public static final ;// #L
+    public static final String 자동차_이름_구분자_패턴 = "\\s*,\\s*"; // #L42
+    public static final int 자동차_이름_길이_제한 = 5; // #L42
+
+    // 0에서 9 사이에서 무작위 값에 대해
+    // 값이 4 이상일 경우, 자동차가 전진한다. // #L44
+    public static final int MIN_VALUE_FORWARD = 0;
+    public static final int MAX_VALUE_FORWARD = 9;
+    public static final int STANDARD_VALUE_FORWARD = 4;
+
+    public static final String 우승자_구분자 = ","; // #L46
+
 }
