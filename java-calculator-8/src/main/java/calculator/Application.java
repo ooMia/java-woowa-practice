150d25dd42673b27d7ab898fecbf7f69ee490aa5
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-12-03 03:45:55 +0900

    feat: implement controller

--- java-calculator-8/src/main/java/calculator/Application.java
+++ java-calculator-8/src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    @SuppressWarnings("java:S106")
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String expression = Console.readLine();
+        var result = new Calculator().add(expression);
+        System.out.printf("결과 : %s", result);
     }
 }
