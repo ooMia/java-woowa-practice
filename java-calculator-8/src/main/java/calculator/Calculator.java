fd887ea26a5f115f1b3568ec7288519e2d267f6b
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-12-03 03:43:31 +0900

    feat(calculator): implement add
    - `testAdd_overflowThrows`

---
+++ java-calculator-8/src/main/java/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package calculator;
+
+import calculator.delimiter.Delimiter;
+
+public class Calculator {
+    private final Delimiter<Integer> delimiter = Delimiter.ofInteger();
+
+    private static int sum(Iterable<Integer> numbers) throws ArithmeticException {
+        int sum = 0;
+        for (var number : numbers) {
+            sum = Math.addExact(sum, number);
+        }
+        return sum;
+    }
+
+    int add(String expression) {
+        var numbers = delimiter.parse(expression);
+        try {
+            return sum(numbers);
+        } catch (ArithmeticException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
